d34a70f591d55de22000d11afd068a15c07dcfd0
Sergey Y. Efremov <mynog@yandex.ru>
2018-10-10 10:54:11 +0300

    Update Gatling scenario (add warm-up requests stage)

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
 ###Search request Gatling test
 
-Нагрузочное тестирование с использованием Gatling'а для [Search request logger'a](https://github.com/efserg/search-request-logger)
+Нагрузочное тестирование с использованием Gatling'а для Search request logger'a
 
 ####Конфигурирование
 Настройка параметров происходит путем редактирования свойств в `conf/performanceTest.conf`
 
 * `url="http://localhost:8080" ` - базовый url
-* `users=1000` - число запросов в секунду
-* `duration=10` - продолжительность теста (в секундах)
 
-При описанной конфигурации будет послано 10000 запросов за 10 секунд (то есть в среднем 1000 запросов в секунду)
+Тестовый сценарий предполагает два этапа
+1. прогрев - несколько запросов с небольшой частотой:
+    * `warmup-users=100` - общее число "прогревочных" запросов 
+    * `warmup-duration=5` - длительность "прогрева" (в секундах)
+2. нагрузочное тестирование с запросами через одинаковые промежутки времени:
+    * `stress-users = 500` - число нагрузочных запросов в секунду
+    * `stress-duration=10`- длительность нагрузочного тестирования (в секундах)
 
 ####Запуск тестирования
-1. Запустите [search request logger](https://github.com/efserg/search-request-logger)
-1. Склонируйте репозиторий `git clone git@github.com:efserg/search-request-gatling.git`
-1. Нужным образом отредактируйте `conf/performanceTest.conf`
-1. Выполните `mvn -Dgatling.simulationClass=BasicSimulation gatling:test` в директории проекта
-1. Gatling отошлет нужное число запросов на создание записи в базе
-1. Отчет по тестированию будет доступен по пути `target\gatling\basicsimulation-<timestamp>\index.html`
+1. Запустить search request logger
+1. Перейти в папку проекта `cd search-request-gatling`
+1. Нужным образом отредактировать `conf/performanceTest.conf`
+1. Выполнить `mvn -Dgatling.simulationClass=BasicSimulation gatling:test` в директории проекта
+1. Gatling отошлет нужное число запросов на создание записей в базе
+1. Отчет по тестированию будет доступен по пути `target\gatling\basicsimulation-<timestamp>\index.html` (путь к отчету будет выведен в конце симуляции)
--- src/test/scala/BasicSimulation.scala
+++ src/test/scala/BasicSimulation.scala
@@ -1,18 +1,3 @@
-/**
- * Copyright 2011-2017 GatlingCorp (http://gatling.io)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 import com.typesafe.config.{Config, ConfigFactory}
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
@@ -25,8 +10,10 @@ class BasicSimulation extends Simulation {
 
   private val config: Config = ConfigFactory.load("performanceTest.conf")
   private val baseUrl = config.getString("url")
-  private val users = config.getInt("users")
-  private val duration = config.getInt("duration")
+  private val stressUsers = config.getInt("stress-users")
+  private val stressDuration = config.getInt("stress-duration")
+  private val warmupUsers = config.getInt("warmup-users")
+  private val warmupDuration = config.getInt("warmup-duration")
 
   val httpConf: HttpProtocolBuilder =
     http
@@ -35,13 +22,17 @@ class BasicSimulation extends Simulation {
 
   val scn: ScenarioBuilder =
     scenario("Search requests logging simulation")
-//      .feed(cities)
       .exec(http("Send search request")
         .post("/add")
-        .body(ElFileBody("user-files/bodies/search-request.json"))
-        .asJSON)
+        .body(ElFileBody("user-files/bodies/search-request.json")).asJSON)
+  //      .exec(_.set("${timestamp}", System.currentTimeMillis() / 1000 + Math.random()))
+  //      .exec(_.set("${uid}", RandomStringUtils.randomAlphabetic(2) + RandomStringUtils.randomNumeric(5)))
+
 
   setUp(
-    scn.inject(constantUsersPerSec(users) during (duration second))
+    scn.inject(
+      rampUsers(warmupUsers) over (warmupDuration second),
+      constantUsersPerSec(stressUsers) during (stressDuration second),
+    )
   ).protocols(httpConf)
 }
